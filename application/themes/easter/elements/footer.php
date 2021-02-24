ae004d6b42c673ab4374dc4939fbea1fc1608951
JR Stanley <jr@jrstn.ly>
2021-02-24 11:42:27 -0600

    Updated footer

--- application/themes/easter/elements/footer.php
+++ application/themes/easter/elements/footer.php
@@ -1,23 +1,16 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <footer id="footer-theme">
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <?php
-                    $a = new GlobalArea('Footer Legal');
-                    $a->display();
-                    ?>
-                </div>
-                <div class="col-sm-3">
-                  <span class="pull-right">
-                      <?php echo Core::make('helper/navigation')->getLogInOutLink() ?>
-                  </span>
-                </div>
-            </div>
-        </div>
-    </section>
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <?php
+              $a = new GlobalArea('Footer Legal');
+              $a->display();
+              ?>
+          </div>
+      </div>
+  </div>
 </footer>
 
 <?php $this->inc('elements/footer_bottom.php');?>
