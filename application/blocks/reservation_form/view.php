4b0bacabee5e42fbfac28d22ac33af64fbdb4a49
JR Stanley <jr@jrstn.ly>
2021-02-28 22:56:32 -0600

    Update form to be responsive

--- application/blocks/reservation_form/view.php
+++ application/blocks/reservation_form/view.php
@@ -18,53 +18,55 @@
 ?>
 
 <div mbsc-form id="reservation">
-	<div class="mbsc-grid">
-		<div class="mbsc-row">
-			<div class="mbsc-col">
-				<label>
-					Name
-					<input mbsc-input id="Name" onKeyup="validateField(this, 'Name is required.')" />
-				</label>
+	<div class="mbsc-form-group">
+		<div class="mbsc-grid">
+			<div class="mbsc-row">
+				<div class="mbsc-col-12 mbsc-col-md-6">
+					<label>
+						Name
+						<input mbsc-input id="Name" onKeyup="validateField(this, 'Name is required.')" />
+					</label>
+				</div>
+				<div class="mbsc-col-12 mbsc-col-md-6">
+					<label>
+						Email
+						<input mbsc-input id="Email" onKeyup="validateField(this, 'Email is required.')" />
+					</label>
+				</div>
 			</div>
-			<div class="mbsc-col">
-				<label>
-					Email
-					<input mbsc-input id="Email" onKeyup="validateField(this, 'Email is required.')" />
-				</label>
-			</div>
-		</div>
-		<div class="mbsc-row">
-			<div class="mbsc-col">
-				<label>
-					Location
-					<select id="Campus" onChange="setServiceTime(), resetServiceTime(), validateField(this, 'Location is required.')">
-						<option disabled selected value></option>
-						<option value="EP">Eden Prairie - Auditorium</option>
-						<option value="Chapel">Eden Prairie - Chapel</option>
-						<option value="CH">Chaska - Auditorium</option>
-					</select>
-				</label>
-			</div>
-			<div class="mbsc-col">
-				<label>
-					Service Time
-					<select id="ServiceTime" onChange="getCount(), validateField(this, 'Service Time is required.')">
+			<div class="mbsc-row">
+				<div class="mbsc-col-12 mbsc-col-md-6">
+					<label>
+						Location
+						<select id="Campus" onChange="setServiceTime(), resetServiceTime(), validateField(this, 'Location is required.')">
+							<option disabled selected value></option>
+							<option value="EP">Eden Prairie - Auditorium</option>
+							<option value="Chapel">Eden Prairie - Chapel</option>
+							<option value="CH">Chaska - Auditorium</option>
+						</select>
+					</label>
+				</div>
+				<div class="mbsc-col-12 mbsc-col-md-6">
+					<label>
+						Service Time
+						<select id="ServiceTime" onChange="getCount(), validateField(this, 'Service Time is required.')">
 
-					</select>
-				</label>
+						</select>
+					</label>
+				</div>
 			</div>
-		</div>
-		<div class="mbsc-row">
-			<div class="mbsc-col">
-				<input id="Count" mbsc-stepper value="1" min="1" max="1" step="1" >
-					Number of people in your party?
-					<span class="mbsc-desc" id="countMessage"></span>
-				</input>
+			<div class="mbsc-row">
+				<div class="mbsc-col">
+					<input id="Count" mbsc-stepper value="1" min="1" max="1" step="1" >
+						Number of people in your party?
+						<span class="mbsc-desc" id="countMessage"></span>
+					</input>
+				</div>
 			</div>
-		</div>
 
-		<div class="submit mbsc-row">
-			 <button mbsc-button id="submitButton" type="submit" onClick="submit()">Submit</button>
+			<div class="submit mbsc-row">
+				<button mbsc-button id="submitButton" type="submit" onClick="submit()">Submit</button>
+			</div>
 		</div>
 	</div>
 </div>
