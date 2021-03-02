e4e4d979f2bd1e691f986db7d77e4d46c06c5ac0
JR Stanley <jr@jrstn.ly>
2021-03-02 07:14:48 -0600

    Fixed nav scroll distance

--- application/blocks/manual_nav/view.js
+++ application/blocks/manual_nav/view.js
@@ -15,6 +15,32 @@
 			$('#menu-button').removeClass('is-active');
 			$('#manual-nav').removeClass('is-active');
 		})
-	})
+	});
+
+	function scrollIfAnchor(href) {
+		if (typeof(href) != "string") {
+			href.preventDefault();
+			href = $(this).attr("href");
+		}
+
+		var fromTop = 60;
+
+		if (href.includes("#")) {
+			const anchor = href.split('#')[1];
+			var $target = $('#'+anchor);
+
+			if($target.length) {
+				$('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+			} else {
+				window.location.href = href;
+			}
+		} else if (href !== "") {
+			window.location.href = href;
+		}
+	}    
+
+	scrollIfAnchor(window.location.hash);
+
+	$("body").on("click", "a", scrollIfAnchor);
 
 })(window, $);
